9ea55bf36a41677d2662f9982a1211f825d75f2d
unknown <sbigselnambuchi@gmail.com>
2022-06-20 10:48:14 +0300

    first and last commit

---
+++ public/browser/mazeAlgorithms/stairDemonstration.js
@@ -0,0 +1,53 @@
+function stairDemonstration(board) {
+  let currentIdX = board.height - 1;
+  let currentIdY = 0;
+  let relevantStatuses = ["start", "target", "object"];
+  while (currentIdX > 0 && currentIdY < board.width) {
+    let currentId = `${currentIdX}-${currentIdY}`;
+    let currentNode = board.nodes[currentId];
+    let currentHTMLNode = document.getElementById(currentId);
+    if (!relevantStatuses.includes(currentNode.status)) {
+      currentNode.status = "wall";
+      board.wallsToAnimate.push(currentHTMLNode);
+    }
+    currentIdX--;
+    currentIdY++;
+  }
+
+  // class Dumstar {
+  //   constructor (_height){
+  //     this.height =_heigh;
+  //     this.width = _height
+  //   }
+  
+  //   get Element(){
+  //     return this.height *this.width;
+  //   }
+  // }
+ 
+  while (currentIdX < board.height - 2 && currentIdY < board.width) {
+    let currentId = `${currentIdX}-${currentIdY}`;
+    let currentNode = board.nodes[currentId];
+    let currentHTMLNode = document.getElementById(currentId);
+    if (!relevantStatuses.includes(currentNode.status)) {
+      currentNode.status = "wall";
+      board.wallsToAnimate.push(currentHTMLNode);
+    }
+    currentIdX++;
+    currentIdY++;
+  }
+  while (currentIdX > 0 && currentIdY < board.width - 1) {
+    let currentId = `${currentIdX}-${currentIdY}`;
+    let currentNode = board.nodes[currentId];
+    let currentHTMLNode = document.getElementById(currentId);
+    if (!relevantStatuses.includes(currentNode.status)) {
+      currentNode.status = "wall";
+      board.wallsToAnimate.push(currentHTMLNode);
+    }
+    currentIdX--;
+    currentIdY++;
+  }
+}
+
+module.exports = stairDemonstration;
+`           `
